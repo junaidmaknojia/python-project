ddee0d7956be84b670c8658c30235fe6f67d32a3
Sean Rader <seanrader@Seans-MacBook-Pro.local>
2021-04-27 10:08:20 -0700

    functioning component

--- react-app/src/components/GlobalChat/index.js
+++ react-app/src/components/GlobalChat/index.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState } from "react";
 import {useSelector} from "react-redux";
 import io from "socket.io-client";
+import MessageDisplay from '../MessageDisplay';
 
 // need user instance (info: url, name)
 
@@ -10,9 +11,7 @@ const socket = io(endPoint);
 
 const GlobalChat = () => {
   const user = useSelector(state => state.session.user);
-  const [ messages, setMessages ] = useState([{
-    name: "start", body: "start messages"
-  }]);
+  const [ messages, setMessages ] = useState([]);
   const [ newMessage, setNewMessage ] = useState('');
   
   const channel = {
@@ -45,18 +44,15 @@ const GlobalChat = () => {
   }
 
   useEffect(() => {
-    socket.emit("join_room", {name: user.username, room:channel.id})
+    socket.emit("join_room", {user_id: user.id, room:channel.id})
     // setMessages([...channel.messages])
   }, [])
 
   return (
     <div>
       {messages.length > 0 &&
-        messages.map(data => (
-          <div key={data.body + Math.random()}>
-            <h1>{data.name}</h1>
-            <p>{data.body}</p>
-          </div>
+        messages.map((data, i) => (
+          <MessageDisplay message={data} key={i} />
         ))}
       <input value={newMessage} name="message" onChange={e => setNewMessage(e.target.value)}/>
       <button onClick={sendMessage}>Send Message</button>
