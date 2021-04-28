21d392539dbeccb71519f0a6f54f07e95b82e0c4
Jason Slade <jason.slade906@gmail.com>
2021-04-27 20:24:09 -0600

    everything seems to be working

--- react-app/src/components/MessageDisplay/index.js
+++ react-app/src/components/MessageDisplay/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import styles from './MessageDisplay.module.CSS';
 
 const MessageDisplay = ({message}) => {
-    console.log(typeof(message.created_at), 'in display')
+    const date = new Date(message.created_at)
 
     return (
         <div className="message_wrapper">
@@ -15,7 +15,7 @@ const MessageDisplay = ({message}) => {
                         {message.user.username}
                     </span>
                     <span className="message_timestamp">
-                        {message.created_at.getTime()}
+                        {date.getTime()}
                     </span>
                     <div className="message_body">
                         <pre className="message_body__text">{message.body}</pre>
