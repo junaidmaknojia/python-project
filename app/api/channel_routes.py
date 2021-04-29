be74a7a7bb87b2fc804ee3d05f14ab3ce315a57c
nic <nicgauer@gmail.com>
2021-04-29 10:32:45 -0500

    heroku build

--- app/api/channel_routes.py
+++ app/api/channel_routes.py
@@ -7,18 +7,18 @@ channel_routes = Blueprint('channel', __name__)
 
 @channel_routes.route('')
 def get_channels():
-  user = User.query.filter(User.id == current_user.id).one()
-  channel_collection = user.channels
-  channels = { "channel": list(map(lambda ch: ch.to_dict(), channel_collection)) }
-  return channels
+    user = User.query.filter(User.id == current_user.id).one()
+    channel_collection = user.channels
+    channels = { "channel": list(map(lambda ch: ch.to_dict(), channel_collection)) }
+    return channels
 
 
 @channel_routes.route('', methods=['POST'])
 def add_channel():
-  data = request.json
-  channel = Channel(**data)
-  user = User.query.filter(User.id == current_user.id).one()
-  channel.users.append(user)
-  db.session.add(channel)
-  db.session.commit()
-  return channel.to_dict()
+    data = request.json
+    channel = Channel(**data)
+    user = User.query.filter(User.id == current_user.id).one()
+    channel.users.append(user)
+    db.session.add(channel)
+    db.session.commit()
+    return channel.to_dict()
---
+++ app/seeds/channels.py
@@ -0,0 +1,12 @@
+from app.models import db, Channel, User
+
+
+def seed_channels():
+    glbl = Channel(type='g', title='Global Chatroom')
+    db.session.add(glbl)
+    db.session.commit()
+
+
+def undo_channels():
+    db.session.execute('TRUNCATE channels RESTART IDENTITY CASCADE;')
+    db.session.commit()
