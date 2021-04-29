d4c437743fa0c0f58435ca733435681e85b35cf5
Jason Slade <jason.slade906@gmail.com>
2021-04-28 23:10:51 -0600

    styled navbar

--- react-app/src/components/Header/index.js
+++ react-app/src/components/Header/index.js
@@ -1,11 +1,35 @@
 import React, {useEffect, useState} from 'react';
 import {useSelector, useDispatch} from 'react-redux'
 import {useParams} from "react-router-dom";
+import {
+  main,
+  leftSide,
+  logoWrapper,
+  logo,
+  center,
+  searchButton,
+  span,
+  rightSide,
+  userPic,
+  img } from "./Header.module.css";
 
 const Header = () => {
   return (
-    <div>
-      <h2>I'm a navbar</h2>
+    <div className={main}>
+      <div className={leftSide}>
+        <div className={logoWrapper}>
+        <img className={logo} src={require("../../assets/logo_purple.svg")}/>
+        </div>
+      </div>
+      <div className={center}>
+        <button className={searchButton}>
+          <span className={span}>Search Snack</span>
+        </button>
+      </div>
+      <div className={rightSide}>
+        <div className={userPic}></div>
+        <img className={img} src={require("../../assets/big.jpg")}/>
+      </div>
     </div>
   )
 }
