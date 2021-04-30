08a2f379bc5d8071960a9d2eadb7f7d59dafecc9
Junaid Maknojia <74396674+junaidmaknojia@users.noreply.github.com>
2021-04-30 15:01:04 -0500

    Update auth_routes.py
    changed "g" to title query

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -78,7 +78,7 @@ def sign_up():
             if upload["url"]:
                 url = upload["url"]
 
-        glbl = Channel.query.filter(Channel.type == 'g').first()
+        glbl = Channel.query.filter(Channel.title == "Global Chatroom").first()
         user = User(
             first_name=form.data['firstName'],
             last_name=form.data['lastName'],
