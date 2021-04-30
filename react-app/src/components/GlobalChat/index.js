745c9c1a43bb0b0e36328292438a41c9277420eb
Sean Rader <seanrader@Seans-MacBook-Pro.local>
2021-04-29 22:22:59 -0700

    Not styled at all rich text editor

--- react-app/src/components/GlobalChat/index.js
+++ react-app/src/components/GlobalChat/index.js
@@ -2,11 +2,15 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import io from "socket.io-client";
 import MessageDisplay from '../MessageDisplay';
+import { EditorState } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg'
+import { convertToHTML } from 'draft-convert';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import styles from './GlobalChat.module.css';
 
 // need user instance (info: url, name)
 
-const endPoint = "https://sn4ck.herokuapp.com/";
+const endPoint = "http://localhost:3000/";
 
 export const socket = io(endPoint);
 
@@ -16,12 +20,25 @@ const GlobalChat = ({ pastMessages }) => {
   const channel_id = currentChannel.id
   const [ messages, setMessages ] = useState([]);
   const [ newMessage, setNewMessage ] = useState('');
+  const [editorState, setEditorState] = useState(
+    () => EditorState.createEmpty(),
+  )
+  const [convertedContent, setConvertedContent] = useState(null);
+  const handleEditorChange = (state) => {
+    setEditorState(state);
+    convertContentToHTML();
+  }
+  const convertContentToHTML = () => {
+    let currentContentAsHTML = convertToHTML(editorState.getCurrentContent());
+    setConvertedContent(currentContentAsHTML);
+  }
 
   socket.on("message", data => {
     setMessages([data, ...messages]);
   });
 
   const sendMessage = () => {
+    console.log('sending the message')
     if (newMessage) {
       socket.emit("message", {
         body: newMessage,
@@ -33,6 +50,7 @@ const GlobalChat = ({ pastMessages }) => {
           picture_url: user.picture_url
         }
       });
+      setConvertedContent('')
       setNewMessage('')
     } else {
       alert("your dumb");
@@ -46,16 +64,28 @@ const GlobalChat = ({ pastMessages }) => {
 
 
   return (
-    <div>
+    <div className={styles.mainWrapper}>
       <div className={styles.messageWrapper}>
       {messages.length > 0 &&
         messages.map((data, i) => (
           <MessageDisplay message={data} key={i} />
           ))}
       </div>
-      <div contenteditable="true"  className={styles.sendMessageBar}>
-        <textarea placeholder={`Message ${currentChannel.title}`} value={newMessage} className={styles.writeTextBox} name="message" onChange={e => setNewMessage(e.target.value)}/>
-      <button className={styles.sendMessageButton} onClick={sendMessage}>=></button>
+      <div className={styles.sendMessageBar}>
+        {/* <textarea placeholder={`Message ${currentChannel.title}`} value={newMessage} className={styles.writeTextBox} 
+        name="message" onChange={e => setNewMessage(e.target.value)}/> */}
+      <div className={styles.textEditorDiv}>
+        <button className={styles.sendMessageButton} onClick={sendMessage}>=></button>
+        <Editor editorState={editorState}
+        onEditorStateChange={handleEditorChange}
+        wrapperClassName={styles.wrapperClass}
+        editorClassName={styles.editorClass}
+        toolbarClassName={styles.toolbarClass}
+        value={newMessage} 
+        placeholder={`   Message ${currentChannel.title}`} 
+        onChange={e => setNewMessage(convertedContent)}
+        />
+      </div>
       </div>
     </div>
   )
