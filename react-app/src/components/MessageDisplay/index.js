83b92c6803507679bcc6c10b070e91d7b069362e
nic <nicgauer@gmail.com>
2021-04-30 10:48:23 -0500

    Minor styling improvements

--- react-app/src/components/MessageDisplay/index.js
+++ react-app/src/components/MessageDisplay/index.js
@@ -2,7 +2,17 @@ import React, { useState, useEffect } from "react";
 import styles from './MessageDisplay.module.css';
 
 const MessageDisplay = ({message}) => {
-    const date = new Date(message.created_at)
+
+    const formattedTime = () => {
+        let date = new Date(message.created_at)
+        let hours = date.getHours()
+        let minutes = date.getMinutes()
+        let ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours: 12
+        minutes = minutes < 10 ? '0' + minutes: minutes;
+        return `${hours}:${minutes} ${ampm}`
+    }
 
     return (
         <div className={styles.message_wrapper}>
@@ -15,7 +25,7 @@ const MessageDisplay = ({message}) => {
                         {message.user.username}
                     </span>
                     <span className={styles.message_timestamp}>
-                        {date.getTime()}
+                        {formattedTime()}
                     </span>
                     <div className={styles.message_body}>
                         <pre className={styles.message_body__text}>{message.body}</pre>
