3e3d8e8b5a0822051c8b50d985e6b665f22a6bd7
Jason Slade <jason.slade906@gmail.com>
2021-06-07 20:06:38 -0600

    added remaining styles and links

--- react-app/src/components/DevModal/index.js
+++ react-app/src/components/DevModal/index.js
@@ -11,22 +11,30 @@ const devs = [
   {
     name: "Jason Slade",
     pic: big,
-    link: "https://www.linkedin.com/in/jason-slade-976a59205/"
+    link: "https://www.linkedin.com/in/jason-slade-976a59205/",
+    github: "https://github.com/Slade-j",
+    profile: "https://www.linkedin.com/in/jason-slade-976a59205/",
   },
   {
     name: "Junaid Maknojia ",
     pic: fancy,
-    link: "https://www.linkedin.com/in/junaidmaknojia/"
+    link: "https://www.linkedin.com/in/junaidmaknojia/",
+    github: "https://github.com/junaidmaknojia",
+    profile: "https://www.linkedin.com/in/junaidmaknojia/",
   },
   {
     name: "Sean Rader",
     pic: rader,
-    link: "https://www.linkedin.com/in/sean-rader-9ba95850/"
+    link: "https://www.linkedin.com/in/sean-rader-9ba95850/",
+    github: "https://github.com/sjrader",
+    profile: "https://www.linkedin.com/in/sean-rader-9ba95850/"
   },
   {
     name: "Nic Gauer",
     pic: nic,
-    link: "https://www.linkedin.com/in/nic-gauer-b5937a20b/"
+    link: "https://www.linkedin.com/in/nic-gauer-b5937a20b/",
+    github: "https://github.com/nicgauer",
+    profile: "https://www.linkedin.com/in/nic-gauer-b5937a20b/",
   }
 
 ]
@@ -36,6 +44,8 @@ const DevModal = () => {
 
   const cancelModal = (e) => {
     e.stopPropagation();
+    (e.target.className === styles.devOverlay ||
+    e.target.className === styles.cancel) &&
     setShowDevs(false);
   }
 
@@ -46,6 +56,9 @@ const DevModal = () => {
           <button className={styles.cancel} onClick={cancelModal}>
           <i className="fas fa-times"></i>
           </button>
+        </div>
+        <div className={styles.header}>
+          <h1 className={styles.theDevs}>{'< The Devs />'}</h1>
         </div>
         <div className={styles.developersContainer}>
           {devs.map(developer => (
--- react-app/src/components/Developer/index.js
+++ react-app/src/components/Developer/index.js
@@ -3,14 +3,38 @@ import styles from './Developer.module.css';
 import { NavLink } from 'react-router-dom';
 
 const Developer = ({ dev }) => {
+
+  //************element holding************
+  // <span className={styles.linkedIn}>GitHub</span>
+  // <span className={styles.linkedIn}>Linkedin</span>
   return (
     <div className={styles.devWrapper}>
-      <div className={styles.imgContainer} styles={{backgroundImage: `url(${dev.pic})`}}>
+      <div className={styles.imgContainer} style={{backgroundImage: `url(${dev.pic})`}}>
+        <img className={styles.image} src={dev.pic}/>
       </div>
       <div className={styles.nameContainer}>
-        <NavLink to={dev.link}>
+        <a href={dev.link} className={styles.nameLink} target="_blank" rel="noopener noreferrer">
           <span className={styles.devName}>{`${dev.name}`}</span>
-        </NavLink>
+        </a>
+        <div className={styles.linkFlexer}>
+          <a href={dev.link} className={styles.linkedInLink} target="_blank" rel="noopener noreferrer">
+            <div className={styles.trying}>
+              <i className="fab fa-linkedin fa-2x"></i>
+              <div className={styles.background} />
+            </div>
+          </a>
+          <a href={dev.github} className={styles.gitHubLink} target="_blank" rel="noopener noreferrer">
+            <div className={styles.gitLogo}>
+              <i className="fab fa-github fa-2x"></i>
+              <div className={styles.gitBackground} />
+            </div>
+          </a>
+          <a href={dev.profile} className={styles.profileLink} target="_blank" rel="noopener noreferrer">
+            <div className={styles.profileLogo}>
+              <i className="fas fa-user fa-2x"></i>
+            </div>
+          </a>
+        </div>
       </div>
     </div>
   )
